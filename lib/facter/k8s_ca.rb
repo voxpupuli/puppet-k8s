fdb9efe6c4dca6165c3aba3ad2f18c4ccd304996
Alexander Olofsson <alexander.olofsson@liu.se>
2021-05-06 20:52:29 +0200

    Fix k8s_ca fact

--- lib/facter/k8s_ca.rb
+++ lib/facter/k8s_ca.rb
@@ -1,6 +1,6 @@
 Facter.add(:k8s_ca) do
   confine { File.exist? '/etc/kubernetes/certs/ca.pem' }
   setcode do
-    Base64.strict_encode(File.read('/etc/kubernetes/certs/ca.pem'))
+    Base64.strict_encode64(File.read('/etc/kubernetes/certs/ca.pem'))
   end
 end
