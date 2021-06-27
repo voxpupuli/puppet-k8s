be7229be92e617b0f99a5ad425203880daf80935
Alexander "Ace" Olofsson <ace@haxalot.com>
2021-06-27 15:42:00 +0200

    Speed up automated CSR signing

--- manifests/server/tls/k8s_sign.pp
+++ manifests/server/tls/k8s_sign.pp
@@ -3,7 +3,7 @@ define k8s::server::tls::k8s_sign(
 ) {
   exec { "Sign ${name} cert":
     path    => ['/usr/local/bin','/usr/bin','/bin'],
-    command => "kubectl --kubeconfig='${kubeconfig}' get csr | grep 'system:node:${name}' | grep Pending | awk '{print \$1}' | xargs -rn1 kubectl --kubeconfig='${kubeconfig}' certificate approve",
+    command => "kubectl --kubeconfig='${kubeconfig}' get csr | grep 'system:node:${name}' | grep Pending | awk '{print \$1}' | xargs -r kubectl --kubeconfig='${kubeconfig}' certificate approve",
     onlyif  => "kubectl --kubeconfig='${kubeconfig}' get csr | grep 'system:node:${name}' | grep Pending",
   }
 }
