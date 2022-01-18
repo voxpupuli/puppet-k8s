a269c8099a0d91c37c1e34370d172bfc3c1d1ccb
Alexander Olofsson <ace@haxalot.com>
2022-01-18 13:48:51 +0100

    Only enable dual-stack with two or more CIDRS

--- manifests/node/kubelet.pp
+++ manifests/node/kubelet.pp
@@ -12,7 +12,7 @@ class k8s::node::kubelet(
   Enum['cert', 'token', 'bootstrap'] $auth = $k8s::node::node_auth,
   Boolean $rotate_server_tls = $auth == 'bootstrap',
   Boolean $manage_firewall = $k8s::node::manage_firewall,
-  Boolean $support_dualstack = $k8s::cluster_cidr =~ Array[Data],
+  Boolean $support_dualstack = $k8s::cluster_cidr =~ Array[Data, 2],
 
   Stdlib::Unixpath $cert_path = $k8s::node::cert_path,
   Stdlib::Unixpath $kubeconfig = '/srv/kubernetes/kubelet.kubeconf',
