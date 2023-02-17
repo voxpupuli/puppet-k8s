3873daf4bf7027fcb893f75da4695fa7e4882b85
Alexander "Ace" Olofsson <ace@haxalot.com>
2023-02-17 14:43:18 +0100

    Fixup tests to work with vox modulesync

--- spec/classes/server/etcd/setup_spec.rb
+++ spec/classes/server/etcd/setup_spec.rb
@@ -29,26 +29,26 @@ describe 'k8s::server::etcd::setup' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it do
+        is_expected.to contain_archive('/var/tmp/etcd-v3.6.0-linux-amd64.tar.gz').with(
+          ensure: 'present',
+          source: 'https://storage.googleapis.com/etcd/v3.6.0/etcd-v3.6.0-linux-amd64.tar.gz',
+          extract: true,
+          extract_command: 'tar xfz %s --strip-components=1',
+          extract_path: '/usr/local/bin',
+          cleanup: true,
+          creates: [ '/usr/local/bin/etcd', '/usr/local/bin/etcdctl' ],
+        ).that_notifies('Service[etcd]')
+      end
+      it { is_expected.to contain_user('etcd') }
+      it { is_expected.to contain_group('etcd') }
+      it { is_expected.to contain_file('/etc/etcd').with_ensure('directory') }
+      it { is_expected.to contain_file('/var/lib/etcd').with_ensure('directory') }
+      it { is_expected.to contain_file('/etc/etcd/etcd.conf') }
+      it { is_expected.to contain_file('/etc/etcd/cluster.conf') }
+      it { is_expected.to contain_systemd__unit_file('etcd.service') }
+      it { is_expected.to contain_service('etcd').with_ensure('running').that_subscribes_to('File[/etc/etcd/etcd.conf]') }
     end
   end
-
-  it do
-    is_expected.to contain_archive('/var/tmp/etcd-v3.6.0-linux-amd64.tar.gz').with(
-      ensure: 'present',
-      source: 'https://storage.googleapis.com/etcd/v3.6.0/etcd-v3.6.0-linux-amd64.tar.gz',
-      extract: true,
-      extract_command: 'tar xfz %s --strip-components=1',
-      extract_path: '/usr/local/bin',
-      cleanup: true,
-      creates: [ '/usr/local/bin/etcd', '/usr/local/bin/etcdctl' ],
-    ).that_notifies('Service[etcd]')
-  end
-  it { is_expected.to contain_user('etcd') }
-  it { is_expected.to contain_group('etcd') }
-  it { is_expected.to contain_file('/etc/etcd').with_ensure('directory') }
-  it { is_expected.to contain_file('/var/lib/etcd').with_ensure('directory') }
-  it { is_expected.to contain_file('/etc/etcd/etcd.conf') }
-  it { is_expected.to contain_file('/etc/etcd/cluster.conf') }
-  it { is_expected.to contain_systemd__unit_file('etcd.service') }
-  it { is_expected.to contain_service('etcd').with_ensure('running').that_subscribes_to('File[/etc/etcd/etcd.conf]') }
 end
