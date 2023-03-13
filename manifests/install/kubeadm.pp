4bcee55cacaf2ab8bf99e9e57ded306c60ad41ef
Robert Waffen <rwaffen@gmail.com>
2023-03-13 10:46:39 +0100

    document ensure parameter better

--- manifests/install/kubeadm.pp
+++ manifests/install/kubeadm.pp
@@ -1,6 +1,6 @@
 # @summary Installs the kubeadm binary
 #
-# @param ensure
+# @param ensure set ensure for installation or deinstallation
 #
 class k8s::install::kubeadm (
   K8s::Ensure $ensure = $k8s::ensure,
--- manifests/install/kubectl.pp
+++ manifests/install/kubectl.pp
@@ -1,6 +1,6 @@
 # @summary Installs the kubectl binary
 #
-# @param ensure
+# @param ensure set ensure for installation or deinstallation
 #
 class k8s::install::kubectl (
   K8s::Ensure $ensure = $k8s::ensure,
