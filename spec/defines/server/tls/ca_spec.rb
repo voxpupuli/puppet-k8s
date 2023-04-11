4a39b62decec5f4131e1c363b246b476d4b2b2b2
Alexander Olofsson <ace@haxalot.com>
2023-04-11 15:19:22 +0200

    Fix CA spec

--- spec/defines/server/tls/ca_spec.rb
+++ spec/defines/server/tls/ca_spec.rb
@@ -28,7 +28,7 @@ describe 'k8s::server::tls::ca' do
         is_expected.to contain_exec('Create namevar CA cert').with(
           command: %r{openssl req -x509 -new -nodes -key '/tmp/ca.key'\s+-days '10000' -out '/tmp/ca.pem' -subj '/CN=namevar'},
           unless: "openssl x509 -CA '/tmp/ca.pem' -CAkey '/tmp/ca.key' -in '/tmp/ca.pem' -noout"
-        ).that_requires('File[/tmp/ca.key]')
+        ).that_subscribes_to('File[/tmp/ca.key]')
       end
 
       it do
