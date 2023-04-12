65cfb8c33b09f6bff4502c7966e81618708dbcd5
Alexander Olofsson <ace@haxalot.com>
2023-04-12 13:57:23 +0200

    Switch to a refreshonly exec for wait online

--- manifests/server/wait_online.pp
+++ manifests/server/wait_online.pp
@@ -2,14 +2,15 @@
 class k8s::server::wait_online {
   # Wait up to 30 seconds for kube-apiserver to start
   exec { 'k8s-apiserver wait online':
-    command   => 'kubectl version',
-    path      => $facts['path'],
-    tries     => 15,
-    try_sleep => 2,
+    command     => 'kubectl version',
+    path        => $facts['path'],
+    refreshonly => true,
+    tries       => 15,
+    try_sleep   => 2,
   }
 
   # Require possibly managed components before checking online state
   Kubeconfig <| title == '/root/.kube/config' |> -> Exec['k8s-apiserver wait online']
   K8s::Binary <| title == 'kubectl' |> -> Exec['k8s-apiserver wait online']
-  Service <| title == 'kube-apiserver' |> -> Exec['k8s-apiserver wait online']
+  Service <| title == 'kube-apiserver' |> ~> Exec['k8s-apiserver wait online']
 }
