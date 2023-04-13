c4526ee87467b0df0750c01fc9c3513de74120c9
Alexander "Ananace" Olofsson <me+github@ananace.dev>
2023-04-13 13:45:00 +0200

    Revert "Include every IP address into a cert's SAN field"

--- manifests/server/etcd.pp
+++ manifests/server/etcd.pp
@@ -33,11 +33,12 @@ class k8s::server::etcd (
     $addn_names = [
       fact('networking.hostname'),
       fact('networking.fqdn'),
+      fact('networking.ip'),
+      fact('networking.ip6'),
       'localhost',
-      $facts.get('networking.interfaces', {}).map |$_,$v| {
-        ($v.get('bindings', []) + $v.get('bindings6', [])).map |$x| { $x.get('address') }
-      }.filter |$x| { !empty($x) },
-    ].flatten.sort.unique
+      '127.0.0.1',
+      '::1',
+    ]
 
     k8s::server::tls::ca {
       default:
