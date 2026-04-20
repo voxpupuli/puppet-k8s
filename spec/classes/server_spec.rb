0d7067ddf466ce7cc6d941f79ee42b68a87e9d9b
Jan Trojak <trojakjan24@gmail.com>
2026-04-20 13:02:54 +0200

    Add test for full setup and full setup with mnaged TLS false

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -5,6 +5,16 @@ require 'spec_helper'
 describe 'k8s::server' do
   let(:pre_condition) do
     <<~PUPPET
+      function puppetdb_query(String[1] $q) {
+        return [
+          {
+            certname => 'node.example.com',
+            parameters => {
+              advertise_client_urls => 'https://node.example.com:2380'
+            }
+          }
+        ]
+      }
       include k8s
     PUPPET
   end
@@ -42,6 +52,42 @@ describe 'k8s::server' do
             proxy_key: '/kube-proxy.key',
           )
         end
+      end
+
+      context 'with full default setup' do
+        let(:params) do
+          {
+            etcd_servers: ['https://localhost:2379'],
+            manage_etcd: true,
+            manage_certs: true,
+            manage_components: true,
+            manage_resources: true,
+            manage_signing: true,
+            manage_kubeadm: true,
+            manage_crictl: true,
+            node_on_server: true,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+      end
+
+      context 'with full setup and manage_certs => false' do
+        let(:params) do
+          {
+            etcd_servers: ['https://localhost:2379'],
+            manage_etcd: true,
+            manage_certs: false,
+            manage_components: true,
+            manage_resources: true,
+            manage_signing: true,
+            manage_kubeadm: true,
+            manage_crictl: true,
+            node_on_server: true,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
       end
     end
   end
