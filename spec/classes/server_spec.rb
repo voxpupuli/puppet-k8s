a03a34d100e6af01c876930c582a8b0086837b55
Jan Trojak <trojakjan24@gmail.com>
2026-04-20 12:03:25 +0200

    Add test for custom node certs in server class

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -21,6 +21,28 @@ describe 'k8s::server' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      context 'with node_on_server and custom node/proxy cert paths' do
+        let(:params) do
+          {
+            node_on_server: true,
+            etcd_servers: ['https://localhost:2379'],
+            node_cert: '/node.crt',
+            node_key: '/node.key',
+            proxy_cert: '/kube-proxy.crt',
+            proxy_key: '/kube-proxy.key',
+          }
+        end
+
+        it do
+          is_expected.to contain_class('k8s::node').with(
+            node_cert: '/node.crt',
+            node_key: '/node.key',
+            proxy_cert: '/kube-proxy.crt',
+            proxy_key: '/kube-proxy.key',
+          )
+        end
+      end
     end
   end
 end
